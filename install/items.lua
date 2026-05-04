6bfcc02e686d64c0d81f339f6cc6a9aeb38a9c9c
Asgaard Developments <146543978+s4t4n667@users.noreply.github.com>
2026-05-04 19:10:32 +0100

    Delete install/items.lua

--- install/items.lua
+++
@@ -1,5 +0,0 @@
-    ['moneywash_key'] = { label = 'Laundromat Key', weight = 1, stack = false, description = 'This can be used to access the Laundromat.' },
-
-    ['moneywash_token'] = { label = 'Laundromat Token', weight = 1, stack = true, description = 'This can be used at the Laundromat to start a wash.' },
-
-    ['wet_bills'] = { label = 'Wet Bills', weight = 0, stack = true, description = 'Needs to be dried and bagged.' },
